a80686311cdf5570149a5c1c3ae440ab5c9a3892
maretol <maretol@outlook.com>
2024-08-06 23:51:10 +0900

    ヘッダーボタンにアイコン追加

--- src/src/components/small/header.tsx
+++ src/src/components/small/header.tsx
@@ -1,17 +1,28 @@
 import Link from 'next/link'
 import { Button } from '../ui/button'
+import { Info, MessageCircle, Tags } from 'lucide-react'
 
 export default function HeaderButtons() {
+  const iconClassName = 'mr-1 w-4 h-4'
   return (
     <div className="flex flex-col sm:flex-row gap-1">
       <Button variant="outline" className="w-full sm:w-48" asChild>
-        <Link href="/tag">Tag Search</Link>
+        <Link href="/tag">
+          <Tags className={iconClassName} />
+          Tags
+        </Link>
       </Button>
       <Button variant="outline" className="w-full sm:w-48" asChild>
-        <Link href="/about">About</Link>
+        <Link href="/about">
+          <Info className={iconClassName} />
+          About
+        </Link>
       </Button>
       <Button variant="outline" className="w-full sm:w-48" asChild>
-        <Link href="/contact">Contact</Link>
+        <Link href="/contact">
+          <MessageCircle className={iconClassName} />
+          Contact
+        </Link>
       </Button>
     </div>
   )
