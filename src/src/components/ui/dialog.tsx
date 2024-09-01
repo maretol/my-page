934cd87e9b072eab5acc2ada3839ed20416cf428
maretol <maretol@outlook.com>
2024-09-01 14:53:33 +0900

    画像モーダルを修正

--- src/src/components/ui/dialog.tsx
+++ src/src/components/ui/dialog.tsx
@@ -35,7 +35,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200',
+        'fixed left-[50%] top-[50%] max-h-full max-w-full w-max z-50 grid translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200',
         className,
       )}
       {...props}
